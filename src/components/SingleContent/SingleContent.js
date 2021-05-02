f3354f9fcb0117068282ad178967643830991f1e
Indu Bakshi <bakshi.webworld@gmail.com>
2021-05-02 20:17:49 +0530

    styling edit

--- src/components/SingleContent/SingleContent.js
+++ src/components/SingleContent/SingleContent.js
@@ -1,6 +1,6 @@
 import { Badge } from '@material-ui/core'
 import React from 'react'
-import { img_500,unavailable } from '../../config/config'
+import { img_300,unavailable } from '../../config/config'
 import './SingleContent.css'
 
 const SingleContent = ({id,poster,title,date,media_type,vote_average}) => {
@@ -8,7 +8,7 @@ const SingleContent = ({id,poster,title,date,media_type,vote_average}) => {
         
         <div className="media">
             <Badge badgeContent={vote_average} color={vote_average>6? 'primary':'secondary' }/>
-                <img className="poster" src={poster?`${img_500}${poster}`:unavailable} alt={title} />
+                <img className="poster" src={poster?`${img_300}${poster}`:unavailable} alt={title} />
                 <b className="title">{title}</b>
                 <div className="subTitle">
                 <span >{media_type === 'tv'?"TV Series":"Movie"}</span>
