56e3402426e9c65308c2d6a34c45bff5c405a03a
Indu Bakshi <bakshi.webworld@gmail.com>
2021-05-02 19:42:44 +0530

    Tv series component update

--- src/components/Pages/Movies/Movies.js
+++ src/components/Pages/Movies/Movies.js
@@ -16,7 +16,7 @@ const Movies = () => {
 
   useEffect(() => {
     axios
-      .get(` https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_genres=${genreforURL}`
+      .get(` https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=true&include_video=false&page=${page}&with_genres=${genreforURL}`
       )
 
       .then((response) => {
--- src/components/Pages/Series/Series.js
+++ src/components/Pages/Series/Series.js
@@ -1,12 +1,58 @@
-import React from 'react'
- 
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import SingleContent from "../../SingleContent/SingleContent";
+import "../Trending/Trending.css";
+import CustomPagination from "../../CustomPagination/CustomPagination";
+import Genre from "../../Genre/Genre";
+import useGenre from "../../Hooks/useGenre";
+
 const Series = () => {
-     return (
-         <div>
-          Series   
-         </div>
-     )
- }
- 
- export default Series
- 
+  const [genre, setGenre] = useState([])
+  const [selectedGenre, setSelectedGenre]= useState([])
+  const [seriesData, setSeriesData] = useState([]);
+  const [page, setPage] = useState(1);
+  const [num, setNum] = useState(10);
+    const genreforURL =useGenre(selectedGenre)
+
+  useEffect(() => {
+    axios
+      .get(` https://api.themoviedb.org/3/discover/tv?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_genres=${genreforURL}`
+      )
+
+      .then((response) => {
+        setSeriesData(response.data.results);
+        setNum(response.data.total_pages);
+      });
+  }, [genreforURL,page]);
+//   console.log("Movie Data", seriesData);
+  return (
+    <div>
+      <span className="pageTitle">Series</span>
+      <Genre
+            genre={genre}
+            setGenre={setGenre}
+            selectedGenre={selectedGenre}
+            setSelectedGenre={setSelectedGenre}
+            setPage={setPage}
+            type="tv"
+      />
+      <div className="trending">
+        {seriesData &&
+          seriesData.map((i) => (
+            <SingleContent
+              key={i.id}
+              id={i.id}
+              poster={i.poster_path}
+              title={i.title || i.name}
+              date={i.first_air_date || i.release_date}
+              media_type="tv"
+              vote_average={i.vote_average}
+            />
+          ))}
+      </div>
+      <CustomPagination setPage={setPage} num={num} />
+    </div>
+  );
+};
+
+export default Series;
