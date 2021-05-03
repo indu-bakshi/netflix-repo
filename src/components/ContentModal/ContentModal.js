6a5953e429f415124146c7f7f026fbd1f7d9c1f4
Indu Bakshi <bakshi.webworld@gmail.com>
2021-05-03 21:55:29 +0530

    model update

---
+++ src/components/ContentModal/ContentModal.js
@@ -0,0 +1,92 @@
+import React, { useEffect, useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+import '../SingleContent/SingleContent.css'
+import axios from 'axios';
+
+import ResponsivePlayer from '../ResponsivePlayer/ResponsivePlayer';
+const useStyles = makeStyles((theme) => ({
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  paper: {
+    width: "90%",
+    height: "80%",
+    backgroundColor: "#000000",
+    border: "1px solid #282c34",
+    borderRadius: 10,
+    color: "white",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(1, 1, 3),
+  },
+}));
+
+export default function ContentModal({children, media_type, id}) {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+  const [content, setContent]= useState([])
+  const [video,setVideo] = useState([])
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  useEffect(()=>{
+    axios
+    .get(`https://api.themoviedb.org/3/${media_type}/${id}/videos?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`)
+    .then(response=>{
+        
+        setVideo(response.data.results[0]?.key)
+        
+    })
+    // eslint-disable-next-line
+  },[])
+  useEffect(()=>{
+    axios
+    .get(`https://api.themoviedb.org/3/${media_type}/${id}?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`)
+    .then(response=>{
+        setContent(response.data)
+    })
+    // eslint-disable-next-line
+  },[])
+
+  
+
+console.log("Video", video)
+console.log("content", content)
+  return (
+    <>
+      <div type="button" onClick={handleOpen} className="media">
+        {children}
+      </div>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <div className={classes.paper}>
+            <h2 id="transition-modal-title">Transition modal</h2>
+            <p id="transition-modal-description">react-transition-group animates me.</p>
+            <ResponsivePlayer video={video}/>
+            
+          </div>
+        </Fade>
+      </Modal>
+    </>
+  );
+}
--- src/components/SingleContent/SingleContent.js
+++ src/components/SingleContent/SingleContent.js
@@ -1,12 +1,13 @@
 import { Badge } from '@material-ui/core'
 import React from 'react'
 import { img_300,unavailable } from '../../config/config'
+import ContentModal from '../ContentModal/ContentModal'
 import './SingleContent.css'
 
 const SingleContent = ({id,poster,title,date,media_type,vote_average}) => {
     return (
         
-        <div className="media">
+        <ContentModal media_type={media_type} id={id}>
             <Badge badgeContent={vote_average} color={vote_average>6? 'primary':'secondary' }/>
                 <img className="poster" src={poster?`${img_300}${poster}`:unavailable} alt={title} />
                 <b className="title">{title}</b>
@@ -14,7 +15,7 @@ const SingleContent = ({id,poster,title,date,media_type,vote_average}) => {
                 <span >{media_type === 'tv'?"TV Series":"Movie"}</span>
                 <span>{date}</span>
                 </div>
-        </div>
+        </ContentModal>
         
     )
 }
